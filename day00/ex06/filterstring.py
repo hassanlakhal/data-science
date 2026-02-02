8525aa7b02ad8d82b206cce75e1397b52e31e604
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-02-02 16:59:36 +0100

    first commit

--- day00/ex06/filterstring.py
+++ day00/ex06/filterstring.py
@@ -1,14 +1,18 @@
 import sys
-
+from ft_filter import ft_filter
 
 def main():
     len_args = len(sys.argv) - 1
     if len_args == 2:
         text = sys.argv[1]
+        list_words = text.split()
         try:
             number = int(sys.argv[2])
         except ValueError:
-            print("Argement number tow not a integer")
+            print("AssertionError: the arguments are bad")
+        new_list = ft_filter(lambda x: len(x) > number, list_words)
+        print(f"{new_list}")
+    
         
 
 if __name__ == "__main__":
