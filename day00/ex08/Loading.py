a8907f1f456186f1f6950b20269a6a2dfbf3d143
Hassan Lakhal <hlakhal-@e2r11p6.1337.ma>
2026-02-03 11:04:44 +0100

    replaced int with round

--- day00/ex08/Loading.py
+++ day00/ex08/Loading.py
@@ -1,14 +1,8 @@
 
-# def generators(lst: range):
-#     for i in lst:
-#         yield(i)
 import os
 
 
-
-
 def ft_tqdm(lst: range):
-    # print(next(range))
     try:
         width = os.get_terminal_size().columns
     except OSError:
@@ -17,7 +11,7 @@ def ft_tqdm(lst: range):
     bar_len = width - len(f"100%|[]| {total}/{total}") - 1
 
     for i in lst:
-        percent = int((i + 1) / total * 100)
+        percent = round((i + 1) / total * 100)
         filled_len = int(bar_len * (i + 1) / total)
 
         bar = '=' * (filled_len - 1) + '>' + ' ' * (bar_len - filled_len)
@@ -28,15 +22,7 @@ def ft_tqdm(lst: range):
 
 from tqdm import tqdm
 from time import sleep
-# for elem in tqdm(range(100)):
-#     sleep(0.1)
-for elem in ft_tqdm(range(100)):
-    sleep(0.16)
-
-# def square_numbers(nums):
-#     for i in nums:
-#         yield(i * i)
-# my_num = square_numbers([1,4,2,5])
-# for i in my_num:
-#     print(i,end='\r')
-# print(next(my_num))
+# for elem in tqdm(range(333)):
+#     sleep(0.9)
+for elem in ft_tqdm(range(333)):
+    sleep(0.9)
