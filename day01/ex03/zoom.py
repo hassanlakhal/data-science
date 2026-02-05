b3e4272feca0a09840bf0712d1e8ab3ac5f68963
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-02-05 22:40:04 +0100

    test zoom

--- day01/ex03/zoom.py
+++ day01/ex03/zoom.py
@@ -1,8 +1,18 @@
 from load_image import ft_load
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+import numpy as np 
 
+def rgb2gray(rgb):
+    return np.dot(rgb,[0.299, 0.587, 0.144])
 
 img = ft_load("animal.jpeg")
-print(type(img))
-imgplot = plt.imshow(img)
+
+# print(type(img))
+# imgplot = plt.imshow(img)
+img2gray = rgb2gray(img)
+
+zoom = img2gray[:3]
+print(np.shape(zoom))
+# img_gray = plt.imshow(zoom, cmap='gray')
+
 plt.show()
