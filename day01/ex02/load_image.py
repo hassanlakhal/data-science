4d1203c13256841f050b99c7ce83ad771e5f62f6
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-02-05 18:16:13 +0100

    ex01 and ex02 is done

---
+++ day01/ex02/load_image.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import numpy as np
+import array
+
+
+def ft_load(path: str) -> array:
+    pxl = []
+    try:
+        img = Image.open(path)
+       
+        if img.format == 'JPEG':
+            pxl = np.array(img)
+            print(f"The shape of image is:  {np.shape(pxl)}")
+        else:
+            print("Only JPG and JPEG formats are supported.")
+    except Exception:
+        print(f"Error while processing file: {path}")
+
+    return pxl
+
+print(ft_load("landscape.j5pg"))
