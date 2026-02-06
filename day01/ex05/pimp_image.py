87ee01d89c3e09b59d6925b1c315024cbc468216
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-02-06 17:12:12 +0100

    fixed ex05

--- day01/ex05/pimp_image.py
+++ day01/ex05/pimp_image.py
@@ -3,18 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def ft_invert(array) -> array:
+    """Inverts the color of the image received."""
     invert = 255 - array
-    # img2invert = plt.imshow(invert)
-    # plt.show()
     return invert
-#your code here
+
 def ft_red(array) -> array:
+    """This function keeps only the red channel of the image."""
     red = array * np.array([1,0,0])
-    # img2red = plt.imshow(red)
-    # plt.show()
     return red
-#your code here
+
 def ft_green(array) -> array:
+    """This function keeps only the green channel of the image."""
     green_img = np.empty_like(array)
     
     green_img[:,:,0] = array[:,:,0] - array[:,:,0]
@@ -22,11 +21,13 @@ def ft_green(array) -> array:
     green_img[:,:,1] = array[:,:,1]
     
     green_img[:,:,2] = array[:,:,2] - array[:,:,2]
-    # img2green = plt.imshow(green_img)
-    # plt.show()
 
-#your code here
+    return green_img 
+
+
 def ft_blue(array) -> array:
+    """This function keeps only the blue channel of the image."""
+
     blue_img = np.empty_like(array)
     
     blue_img[:,:,0] = 0
@@ -34,14 +35,12 @@ def ft_blue(array) -> array:
     blue_img[:,:,1] = 0
     
     blue_img[:,:,2] = array[:,:,2]
-    # img2blue = plt.imshow(blue_img)
-    # plt.show()
+    return blue_img
 
-#your code here
 def ft_grey(array) -> array:
+
+    """Applies a grayscale filter to the image received."""
     combined_sum = np.sum(array, axis=2)
     grey_channel = combined_sum / 3
     res = np.stack([grey_channel, grey_channel, grey_channel], axis=2).astype(np.uint8)
-    print(res)
-    plt.imshow(res)
-    plt.show()
+    return res
