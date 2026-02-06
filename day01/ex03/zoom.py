ad942b4c593b79129c1b35a3098ce0e727593ffd
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-02-06 12:58:50 +0100

    fixed zoom

--- day01/ex03/zoom.py
+++ day01/ex03/zoom.py
@@ -5,14 +5,27 @@ import numpy as np
 def rgb2gray(rgb):
     return np.dot(rgb,[0.299, 0.587, 0.144])
 
-img = ft_load("animal.jpeg")
 
-# print(type(img))
-# imgplot = plt.imshow(img)
-img2gray = rgb2gray(img)
+def main():
+    try:
+        img = ft_load("animal.jpeg")
+        img2gray = rgb2gray(img)
+        print(img)
+        
+        h, w = np.shape(img2gray)
+        zw, zh = 400, 400
+        eh = round((h  - zh) // 2)
+        ew = round((w - zw) // 2)
 
-zoom = img2gray[:3]
-print(np.shape(zoom))
-# img_gray = plt.imshow(zoom, cmap='gray')
+        zoom = img2gray[eh:eh + zh, ew:ew + zw]
+        print(f"New shape after slicing: {np.shape(zoom)}")
+        print(zoom)
 
-plt.show()
+        img_gray = plt.imshow(zoom, cmap='gray')
+        plt.show()
+    except Exception as e:
+        exit(f"Exception {e}")
+
+
+if __name__ == '__main__':
+    main()
