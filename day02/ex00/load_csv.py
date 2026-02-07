b90431aac1312c0d314394373c77f709e10e59a4
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-02-07 14:38:02 +0100

    ex00 is done

--- day02/ex00/load_csv.py
+++ day02/ex00/load_csv.py
@@ -2,17 +2,6 @@ import pandas as pd
 from pandas import DataFrame
 
 def load(path: str) -> DataFrame:
-    df = pd.DataFrame(
-        {
-        "Name": [
-            "Braund, Mr. Owen Harris",
-            "Allen, Mr. William Henry",
-            "Bonnell, Miss Elizabeth",
-        ],
-        "Age": [22, 35, 58],
-        "Sex": ["male", "male", "female"],
-    }
-    )
+    df = pd.read_csv(path)
+    print(f"Loading dataset of dimensions  {titanic.shape}")
     return df
-
-print(load("test"), type(load("test")))
