f1d7d59430f751b833baafdaa1a57e33c4770fbe
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-02-08 14:35:09 +0100

    fixed Interrupted

--- day02/ex01/aff_life.py
+++ day02/ex01/aff_life.py
@@ -1,22 +1,29 @@
 import matplotlib.pyplot as plt 
 from load_csv import load
+import sys
 
 from matplotlib.ticker import EngFormatter
+def main():
+    df = load("life_expectancy_years.csv")
 
-df = load("life_expectancy_years.csv")
+    df = df.set_index("country")
+    morocco = df.loc['Morocco']
+    x = morocco.index 
+    y = morocco.values
 
+    plt.figure(figsize=(5, 5))
 
-df = df.set_index("country")
-morocco = df.loc['Morocco']
-x = morocco.index 
-y = morocco.values
+    plt.plot(x,y)
+    plt.xlabel("Year")
+    plt.xticks(morocco.index[::40])
+    plt.ylabel("Life expectancy")
+    plt.title("Morocco Life expectancy Projections")
 
-plt.figure(figsize=(5, 5))
+    plt.show()
 
-plt.plot(x,y)
-plt.xlabel("Year")
-plt.xticks(morocco.index[::40])
-plt.ylabel("Life expectancy")
-plt.title("Morocco Life expectancy Projections")
-
-plt.show()
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        sys.exit(130)
